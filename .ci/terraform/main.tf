1c406b3255c55d716358d3a2044fe91c05442fbd
Ashen Gunaratne <mail@ashenm.ml>
2022-05-25 18:09:12 +0000

    Add staging Heroku app

--- .ci/terraform/main.tf
+++ .ci/terraform/main.tf
@@ -12,6 +12,17 @@ provider "heroku" {
   api_key = var.heroku_api_key
 }
 
+resource "heroku_app" "staging" {
+  acm                   = false
+  buildpacks            = ["heroku/nodejs"]
+  config_vars           = var.configuration
+  internal_routing      = false
+  name                  = "starscream"
+  region                = "us"
+  sensitive_config_vars = var.secrets
+  stack                 = "heroku-20"
+}
+
 resource "heroku_app" "production" {
   acm                   = false
   buildpacks            = ["heroku/nodejs"]

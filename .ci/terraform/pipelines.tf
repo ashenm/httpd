160728befe3fdf870b8dbdab352f91490b4e87ab
Ashen Gunaratne <mail@ashenm.ml>
2022-05-25 18:09:12 +0000

    Add staging Heroku app

--- .ci/terraform/pipelines.tf
+++ .ci/terraform/pipelines.tf
@@ -2,6 +2,12 @@ resource "heroku_pipeline" "pipeline" {
   name = "httpd"
 }
 
+resource "heroku_pipeline_coupling" "staging" {
+  app      = heroku_app.staging.id
+  pipeline = heroku_pipeline.pipeline.id
+  stage    = "staging"
+}
+
 resource "heroku_pipeline_coupling" "production" {
   app      = heroku_app.production.id
   pipeline = heroku_pipeline.pipeline.id

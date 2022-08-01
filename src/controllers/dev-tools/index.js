ac3ae726b36b4ae35369609cbdb2224e6b308a79
Ashen Gunaratne <mail@ashenm.ml>
2022-08-01 15:02:56 +0000

    Add dev-tools timeout route

--- src/controllers/dev-tools/index.js
+++ src/controllers/dev-tools/index.js
@@ -19,8 +19,16 @@ router.all('/:status', function (request, response, next) {
   request.params.status in configuration ? response.status(configuration[request.params.status].statusCode).json(dispatch.success(configuration[request.params.status].statusCode, request.body)) : next();
 });
 
-router.all('/timeout', function (_request, _response, _next) {
-  return;
+router.all('/timeout', function (request, response, _next) {
+  const delay = parseInt(request.query.ms);
+
+  if (delay !== delay) {
+    return;
+  }
+
+  setTimeout(function () {
+    response.status(200).json(dispatch.success(200));
+  }, delay);
 });
 
 module.exports = router;
